a8394bd5b14c7aef1a92ec7667ea0c206a22908a
quadratic <61506122+mlpro1122@users.noreply.github.com>
2024-04-29 21:43:19 +0100

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install -q -U torch=='2.0.0'
+
+
+# In[2]:
+
+
+pip install -q -U accelerate=='0.25.0' peft=='0.7.1' bitsandbytes=='0.41.3.post2' trl=='0.7.4'
+
+
+# In[3]:
+
+
+pip install -q -U transformers einops
+
+
+# In[4]:
+
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+# In[5]:
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# In the following cell there are all the other imports for running the notebook
+
+# In[6]:
+
+
+import numpy as np
+import pandas as pd
+import os
+from tqdm import tqdm
+import bitsandbytes as bnb
+import torch
+import torch.nn as nn
+import transformers
+from datasets import Dataset
+from peft import LoraConfig, PeftConfig
+from trl import SFTTrainer
+from transformers import (AutoModelForCausalLM, 
+                          AutoTokenizer, 
+                          BitsAndBytesConfig, 
+                          TrainingArguments, 
+                          pipeline, 
+                          logging)
+from sklearn.metrics import (accuracy_score, 
+                             classification_report, 
+                             confusion_matrix)
+from sklearn.model_selection import train_test_split
+
+
+# In[7]:
+
+
+filename = "../input/sentiment-analysis-for-financial-news/all-data.csv"
+
+df = pd.read_csv(filename, 
+                 names=["sentiment", "text"],
+                 encoding="utf-8", encoding_errors="replace")
+
+X_train = list()
+X_test = list()
+for sentiment in ["positive", "neutral", "negative"]:
+    train, test  = train_test_split(df[df.sentiment==sentiment], 
+                                    train_size=300,
+                                    test_size=300, 
+                                    random_state=42)
+    X_train.append(train)
+    X_test.append(test)
+
+X_train = pd.concat(X_train).sample(frac=1, random_state=10)
+X_test = pd.concat(X_test)
+
+eval_idx = [idx for idx in df.index if idx not in list(train.index) + list(test.index)]
+X_eval = df[df.index.isin(eval_idx)]
+X_eval = (X_eval
+          .groupby('sentiment', group_keys=False)
+          .apply(lambda x: x.sample(n=50, random_state=10, replace=True)))
+X_train = X_train.reset_index(drop=True)
+
+def generate_prompt(data_point):
+    return f"""The sentiment of the following phrase: '{data_point["text"]}' is 
+            \n\n Positive
+            \n Negative
+            \n Neutral
+            \n Cannot be determined
+            \n\nSolution: The correct option is {data_point["sentiment"]}""".strip()
+
+def generate_test_prompt(data_point):
+    return f"""The sentiment of the following phrase: '{data_point["text"]}' is 
+            \n\n Positive
+            \n Negative
+            \n Neutral
+            \n Cannot be determined
+            \n\nSolution: The correct option is""".strip()
+
+X_train = pd.DataFrame(X_train.apply(generate_prompt, axis=1), 
+                       columns=["text"])
+X_eval = pd.DataFrame(X_eval.apply(generate_prompt, axis=1), 
+                      columns=["text"])
+
+y_true = X_test.sentiment
+X_test = pd.DataFrame(X_test.apply(generate_test_prompt, axis=1), columns=["text"])
+
+train_data = Dataset.from_pandas(X_train)
+eval_data = Dataset.from_pandas(X_eval)
+
+
+# In[8]:
+
+
+def evaluate(y_true, y_pred):
+    labels = ['positive', 'neutral', 'negative']
+    mapping = {'positive': 2, 'neutral': 1, 'none':1, 'negative': 0}
+    def map_func(x):
+        return mapping.get(x, 1)
+    
+    y_true = np.vectorize(map_func)(y_true)
+    y_pred = np.vectorize(map_func)(y_pred)
+    
+    # Calculate accuracy
+    accuracy = accuracy_score(y_true=y_true, y_pred=y_pred)
+    print(f'Accuracy: {accuracy:.3f}')
+    
+    # Generate accuracy report
+    unique_labels = set(y_true)  # Get unique labels
+    
+    for label in unique_labels:
+        label_indices = [i for i in range(len(y_true)) 
+                         if y_true[i] == label]
+        label_y_true = [y_true[i] for i in label_indices]
+        label_y_pred = [y_pred[i] for i in label_indices]
+        accuracy = accuracy_score(label_y_true, label_y_pred)
+        print(f'Accuracy for label {label}: {accuracy:.3f}')
+        
+    # Generate classification report
+    class_report = classification_report(y_true=y_true, y_pred=y_pred)
+    print('\nClassification Report:')
+    print(class_report)
+    
+    # Generate confusion matrix
+    conf_matrix = confusion_matrix(y_true=y_true, y_pred=y_pred, labels=[0, 1, 2])
+    print('\nConfusion Matrix:')
+    print(conf_matrix)
+
+
+# In[9]:
+
+
+model_name = "microsoft/phi-2"
+
+compute_dtype = getattr(torch, "float16")
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=False,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=compute_dtype,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    trust_remote_code=True,
+    device_map="auto",
+    quantization_config=bnb_config, 
+)
+
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, 
+                                          trust_remote_code=True,
+                                         )
+tokenizer.pad_token = tokenizer.eos_token
+
+
+# In[10]:
+
+
+def predict(X_test, model, tokenizer):
+    y_pred = []
+    for i in tqdm(range(len(X_test))):
+        prompt = X_test.iloc[i]["text"]
+        pipe = pipeline(task="text-generation", 
+                        model=model, 
+                        tokenizer=tokenizer,
+                        max_new_tokens = 3, 
+                        temperature = 0.0,
+                       )
+        result = pipe(prompt, pad_token_id=pipe.tokenizer.eos_token_id)
+        answer = result[0]['generated_text'].split("The correct option is")[-1].lower()
+        if "positive" in answer:
+            y_pred.append("positive")
+        elif "negative" in answer:
+            y_pred.append("negative")
+        elif "neutral" in answer:
+            y_pred.append("neutral")
+        else:
+            y_pred.append("none")
+    return y_pred
+
+
+# At this point, we are ready to test the Phi-2 model and see how it performs on our problem without any fine-tuning. This allows us to get insights on the model itself and establish a baseline.
+
+# In[11]:
+
+
+y_pred = predict(X_test, model, tokenizer)
+
+
+# In the following cell, we evaluate the results. There is little to be said, it is performing really terribly because the 7b-hf model tends to just predict a neutral sentiment and seldom it detects positive or negative sentiment.
+
+# In[12]:
+
+
+evaluate(y_true, y_pred)
+
+
+# In[13]:
+
+
+import re
+
+def get_num_layers(model):
+    numbers = set()
+    for name, _ in model.named_parameters():
+        for number in re.findall(r'\d+', name):
+            numbers.add(int(number))
+    return max(numbers)
+
+def get_last_layer_linears(model):
+    names = []
+    
+    num_layers = get_num_layers(model)
+    for name, module in model.named_modules():
+        if str(num_layers) in name and not "encoder" in name:
+            if isinstance(module, torch.nn.Linear):
+                names.append(name)
+    return names
+
+
+# In[14]:
+
+
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=16,
+    target_modules=["Wqkv", "out_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+
+training_arguments = TrainingArguments(
+    output_dir="logs",
+    num_train_epochs=4,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=8, 
+    optim="paged_adamw_32bit",
+    save_steps=0,
+    logging_steps=25,
+    learning_rate=2e-4,
+    weight_decay=0.001,
+    fp16=True,
+    bf16=False,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=eval_data,
+    peft_config=peft_config,
+    dataset_text_field="text",
+    tokenizer=tokenizer,
+    args=training_arguments,
+    packing=False,
+    max_seq_length=512,
+)
+
+
+# The following code will train the model using the trainer.train() method and then save the trained model to the trained-model directory. Using The standard GPU P100 offered by Kaggle, the training should be quite fast.
+
+# In[15]:
+
+
+# Train model
+trainer.train()
+
+# Save trained model
+trainer.model.save_pretrained("trained-model")
+
+
+# Afterwards, loading the TensorBoard extension and start TensorBoard, pointing to the logs/runs directory, which is assumed to contain the training logs and checkpoints for your model, will allow you to understand how the models fits during the training.
+
+# In[16]:
+
+
+get_ipython().run_line_magic('load_ext', 'tensorboard')
+get_ipython().run_line_magic('tensorboard', '--logdir logs/runs')
+
+
+# In[17]:
+
+
+y_pred = predict(X_test, model, tokenizer)
+evaluate(y_true, y_pred)
+
+
+# In[18]:
+
+
+evaluation = pd.DataFrame({'text': X_test["text"], 
+                           'y_true':y_true, 
+                           'y_pred': y_pred},
+                         )
+evaluation.to_csv("test_predictions.csv", index=False)
+
